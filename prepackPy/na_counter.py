4275d60d0ddfd7b33e96d6871340603ab5b4b1bf
Anthony Chiodo <apchiodo@lakeheadu.ca>
2019-02-14 14:33:19 -0800

    Added example.

--- prepackPy/na_counter.py
+++ prepackPy/na_counter.py
@@ -5,7 +5,8 @@ import pandas as pd
 
 def na_counter(X, col_index=None):
     """
-    Summarise the missing data in a dataset
+    This function takes in tabular data and counts missing columnwise and summarizes the
+    information in a dictionary.
 
     Parameters
     ----------
@@ -19,6 +20,17 @@ def na_counter(X, col_index=None):
         a summary dictionary (key: value)
         key = a column index of X that has missing values
         value = a list (NA counts of missing values in each column)
+
+    Example
+    -------
+    >>> import numpy as np
+    >>> from prepackPy import na_counter as na
+    >>> X = np.array([[-1, np.nan], [np.nan, np.nan], [1, np.nan], [1, 1]])
+
+    >>> na.na_counter(X, col_index=[0,1])
+    {'column': [0, 1], 'nans': [1, 3]}
+
+
     """
 
     # input type check
@@ -40,7 +52,7 @@ def na_counter(X, col_index=None):
         if max(col_index) > X.shape[1] -1 or min(col_index) < 0:
             raise ValueError("At least one value in target_index list out of bounds")
 
-    ### Function
+    ### function
     columns = X.shape[1]
     na_dict = {'column':[], 'nans':[]}
     X_na = np.asarray(X)
